b9170808cf96c1d069e7b118dbe873bfa670201b
rizkisenjani <rizkisenjani@gmail.com>
2025-12-20 10:05:41 +0800

    Cleaned up views.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,18 +2,6 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django import forms
 
-from . import F3F5-0C9Eutil
-
-class SearchForm(forms.Form):
-    form = forms.CharField(label="", max_length=100)
-
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries(),
-        "form": SearchForm(),from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
-from django import forms
-
 from . import util
 
 class SearchForm(forms.Form):
@@ -22,43 +10,9 @@ class SearchForm(forms.Form):
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
-        "form": SearchForm(),from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
-from django import forms
-
-from . import util
-
-class SearchForm(forms.Form):
-    form = forms.CharField(label="", max_length=100)
-
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries(),
-    })  
-
-def entry(request, title):
-    return render(request, "encyclopedia/entry.html", {
-        "title": title,
-        "content": util.get_entry(title),
         "form": SearchForm(),
     })
 
-def search(request):
-    if request.method == "POST":
-        form = SearchForm(request.POST)
-    })  
-
-def entry(request, title):
-    return render(request, "encyclopedia/entry.html", {
-        "title": title,
-        "content": util.get_entry(title),
-        "form": SearchForm(),
-    })
-
-def search(request):
-    if request.method == "POST":
-        form = SearchForm(request.POST)
-    })  
 
 def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
